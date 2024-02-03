f7a07f43b25276235ad09633336918a5cf34de47
frankiep84 <frankpardoel@gmail.com>
2024-02-03 20:45:58 +0100

    bond price function update

--- R/bondPrice.R
+++ R/bondPrice.R
@@ -1,1 +1,9 @@
 # Determine the bond price using a Affine Process
+bondprc <- function(p, r, ttm, y) {
+  cf <- c(rep(p * r, ttm - 1), p * (1 + r))
+  cf <- data.frame(cf)
+  cf$t <- as.numeric(rownames(cf))
+  cf$pv_factor <- 1 / (1 + y)^cf$t
+  cf$pv <- cf$cf * cf$pv_factor
+  sum(cf$pv)
+}
